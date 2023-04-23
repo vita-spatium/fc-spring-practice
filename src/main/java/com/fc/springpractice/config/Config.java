927f1bb40f2cfa9e7e6b0af9bfccc7ab4c432c5a
Andy Choi <uguyz@naver.com>
2023-04-23 12:08:40 +0900

    Config #1 spring config Spring 기초 설정 루트 패키지 이하의 빈을 스캔하도록 설정

---
+++ src/main/java/com/fc/springpractice/config/Config.java
@@ -0,0 +1,10 @@
+package com.fc.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fc.springpractice")
+@Configuration
+public class Config {
+
+}
