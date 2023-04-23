cb7864768942301a444a17dd89713548288e0869
Andy Choi <uguyz@naver.com>
2023-04-23 14:12:07 +0900

    Apply #1 spring webmvc @EnableWebMvc 추가로 스프링 Web Mvc 기능 지원 설정

--- src/main/java/com/fc/springpractice/config/Config.java
+++ src/main/java/com/fc/springpractice/config/Config.java
@@ -2,7 +2,9 @@ package com.fc.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.fc.springpractice")
 @Configuration
 public class Config {
